89311e51d7d88877d568d3c43d246be04d5a30f1
Cintia Souza <cintiasds1@gmail.com>
2023-05-26 13:09:24 -0300

    em constr

--- formulario2/src/app/app.component.ts
+++ formulario2/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -20,8 +21,9 @@ adress:String='';
     this.itens.push(this.id)
   }
 
-  onDelete(index: number){
-    this.itens.splice(index, 1);
+  onSubmit(f:NgForm){
+    console.log(f.value)
   }
+
 
 }
